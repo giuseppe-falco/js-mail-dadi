e4ba7c24ca90ce4107fa1e9aa73ad650f4efdb43
joseph <giuseppe.falco000@gmail.com>
2020-08-24 16:00:19 +0200

    mail done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-//richiesta inserimento mail
+// richiesta inserimento mail
 var email = prompt("Inserisci la tua mail");
-//array database mail
+// array database mail
 var mailAccepted = ["mail@mail.com", "giuseppe.falco@email.it", "mail@email.it"];
 
-if (email = mailAccepted){
-    alert("cioao")
-}else {
-    alert("coglione")
+
+// controllo email nell'array
+var login = false;
+
+for (var i = 0; i < mailAccepted.length; i++) {
+  if (email === mailAccepted[i]) {
+    login = true;
+  }
+}
+
+// stampa errore 
+
+if (login == true) {
+  alert("Accesso confermato");
+} else {
+  alert("Accesso negato");
 }
