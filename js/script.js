0221a0549d4e1880e16c9bba7bbf3740000c29a6
joseph <giuseppe.falco000@gmail.com>
2020-08-24 15:45:33 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//richiesta inserimento mail
+var email = prompt("Inserisci la tua mail");
+//array database mail
+var mailAccepted = ["mail@mail.com", "giuseppe.falco@email.it", "mail@email.it"];
+
+if (email = mailAccepted){
+    alert("cioao")
+}else {
+    alert("coglione")
+}
