a619af4026850fc206267f03250f8a1567874310
joseph <giuseppe.falco000@gmail.com>
2020-08-24 17:33:37 +0200

    added button

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,20 +1,30 @@
-//genera numeri casuali
-var numberPlayer = Math.floor(Math.random() * 6 + 1);
-var numberComputer = Math.floor(Math.random() * 6 + 1);
+var button = document.getElementById("button")
+button.addEventListener('click',
+    function() {
+        
+        //genera numeri casuali
+        var numberPlayer = Math.floor(Math.random() * 6 + 1);
+        var numberComputer = Math.floor(Math.random() * 6 + 1);
+        
+        //stampa risultati
+        document.getElementById("numberPlayer").innerHTML = numberPlayer;
+        document.getElementById("numberComputer").innerHTML = numberComputer;
+        
+        //confronto numeri
+        var winner = document.getElementById("winner");
+        var winnerBox= document.getElementById("winnerBox");
+        if (numberPlayer > numberComputer) {
+            winner.innerHTML = "Hai vinto!";
+            winnerBox.classList.add("winnerGreen");
+        } else if (numberPlayer == numberComputer) {
+            winner.innerHTML = "Avete pareggiato!";
+            winnerBox.classList.add("winnerOrange");
+        } else {
+            winner.innerHTML = "Hai perso!";
+            winnerBox.classList.remove("winnerOrange");
+            winnerBox.classList.remove("winnerGreen");
 
-//stampa risultati
-document.getElementById("numberPlayer").innerHTML = numberPlayer;
-document.getElementById("numberComputer").innerHTML = numberComputer;
+        }
 
-//confronto numeri
-var winner = document.getElementById("winner");
-var winnerBox= document.getElementById("winnerBox");
-if (numberPlayer > numberComputer) {
-    winner.innerHTML = "Hai vinto!";
-    winnerBox.className += "winnerGreen";
-} else if (numberPlayer == numberComputer) {
-    winner.innerHTML = "Avete pareggiato!";
-    winnerBox.className += "winnerOrange";
-} else {
-    winner.innerHTML = "Hai perso!";
-}
+    }
+)
